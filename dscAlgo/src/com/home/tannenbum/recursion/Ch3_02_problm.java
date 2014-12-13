aca4db434582056674218fccf5b7237f2589d3d9
abhinav-ashok <ashok.abhinav@yahoo.in>
2014-12-14 00:21:10 +0530

    tannebum package added

---
+++ dscAlgo/src/com/home/tannenbum/recursion/Ch3_02_problm.java
@@ -0,0 +1,12 @@
+package com.home.tannenbum.recursion;
+
+public class Ch3_02_problm {
+	
+	public static int fact(int n){
+		if( n == 1 ||n == 0)
+			return 1;
+		
+		return n*fact(n-1);
+	}
+
+}
---
+++ dscAlgo/test/com/home/tannenbum/recursion/TestCh3_02_problm.java
@@ -0,0 +1,5 @@
+package com.home.tannenbum.recursion;
+
+public class TestCh3_02_problm {
+
+}
